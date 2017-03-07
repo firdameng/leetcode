639ca8404951273ecc266dddd9f11b82d538d6ad
firdameng <15623780837@163.com>
2017-03-07 10:43:55 +0800

    update it by bit operation

--- Hamming-Distance.c
+++ Hamming-Distance.c
@@ -1,28 +1,13 @@
-class Solution {
-public:
-    int a[32],b[32];
-    void int2binary(int k,int a[]){
-        if(k == 0 || k == 1)
-        {
-            a[0] = k;
-            return;
-        }
-        int i = 0, m;
-        while(k != 1)
-        {
-            m = k%2;
-            a[i++] = m;
-            k = k/2;
-        }
-        a[i] = 1;
+int hammingDistance(int x, int y) {
+    int k = x^y;
+    if(k == 0 || k == 1)
+            return k;
+    int count=1;
+    while(k != 1)
+    {
+        if(k%2)
+            count++;
+        k = k/2;
     }
-    int hammingDistance(int x, int y) {
-        int count=0 ;
-        int2binary(x,a);
-        int2binary(y,b);
-        for(int i=0; i < 32; i++)
-            if(a[i] != b[i])
-                count++;
-        return count;
-    }
-};
+    return count;
+}
